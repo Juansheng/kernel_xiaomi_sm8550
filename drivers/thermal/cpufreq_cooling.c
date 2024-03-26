3616f5326654cb4519b264a7a9364234e42cc42a
Vincent Guittot <vincent.guittot@linaro.org>
2024-03-26 10:16:14 +0100

    sched/freq: Remove arch_update_thermal_pressure()
    arch_update_thermal_pressure() aims to update fast changing signal which
    should be averaged using PELT filtering before being provided to the
    scheduler which can't make smart use of fast changing signal.
    cpufreq now provides the maximum freq_qos pressure on the capacity to the
    scheduler, which includes cpufreq cooling device. Remove the call to
    arch_update_thermal_pressure() in cpufreq cooling device as this is
    handled by cpufreq_get_pressure().
    
    Change-Id: I3027fc016616d010c8668fb9b1ff4c65af87b302
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Dhruva Gole <d-gole@ti.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Link: https://lore.kernel.org/r/20240326091616.3696851-4-vincent.guittot@linaro.org

--- drivers/thermal/cpufreq_cooling.c
+++ drivers/thermal/cpufreq_cooling.c
@@ -465,9 +465,7 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 				 unsigned long state)
 {
 	struct cpufreq_cooling_device *cpufreq_cdev = cdev->devdata;
-	struct cpumask *cpus;
 	unsigned int frequency;
-	unsigned long max_capacity, capacity;
 	int ret;
 
 	/* Request state should be less than max_level */
@@ -483,11 +481,6 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 	ret = freq_qos_update_request(&cpufreq_cdev->qos_req, frequency);
 	if (ret >= 0) {
 		cpufreq_cdev->cpufreq_state = state;
-		cpus = cpufreq_cdev->policy->related_cpus;
-		max_capacity = arch_scale_cpu_capacity(cpumask_first(cpus));
-		capacity = frequency * max_capacity;
-		capacity /= cpufreq_cdev->policy->cpuinfo.max_freq;
-		arch_set_thermal_pressure(cpus, max_capacity - capacity);
 		ret = 0;
 	}
 
