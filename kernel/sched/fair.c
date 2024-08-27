44a10966a123b3c12de87a84e5ad0724171be0d7
Chen Yu <yu.c.chen@intel.com>
2024-08-27 19:26:07 +0800

    sched/pelt: Use rq_clock_task() for hw_pressure
    commit 97450eb90965 ("sched/pelt: Remove shift of thermal clock")
    removed the decay_shift for hw_pressure. This commit uses the
    sched_clock_task() in sched_tick() while it replaces the
    sched_clock_task() with rq_clock_pelt() in __update_blocked_others().
    This could bring inconsistence. One possible scenario I can think of
    is in ___update_load_sum():
    
      u64 delta = now - sa->last_update_time
    
    'now' could be calculated by rq_clock_pelt() from
    __update_blocked_others(), and last_update_time was calculated by
    rq_clock_task() previously from sched_tick(). Usually the former
    chases after the latter, it cause a very large 'delta' and brings
    unexpected behavior.
    
    Fixes: 97450eb90965 ("sched/pelt: Remove shift of thermal clock")
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Hongyan Xia <hongyan.xia2@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20240827112607.181206-1-yu.c.chen@intel.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8634,9 +8634,10 @@ static bool __update_blocked_others(struct rq *rq, bool *done)
 
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
 
+	/* thermal_pressure doesn't care about invariance */
 	decayed = update_rt_rq_load_avg(now, rq, curr_class == &rt_sched_class) |
 		  update_dl_rq_load_avg(now, rq, curr_class == &dl_sched_class) |
-		  update_thermal_load_avg(now, rq, thermal_pressure) |
+		  update_thermal_load_avg(rq_clock_task(rq), rq, thermal_pressure) |
 		  update_irq_load_avg(rq, 0);
 
 	if (others_have_blocked(rq))
